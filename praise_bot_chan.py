42d742b1cd081d99ddb200340e79aaebb55cfeba
mkan0141 <kanzume0141@gmail.com>
2018-06-05 17:30:50 +0900

    update: 連続何日contributeしているか数える機能を追加

--- praise_bot_chan.py
+++ praise_bot_chan.py
@@ -1,12 +1,9 @@
-import requests
-import csv
+import pandas as pd
 import re
+import requests
 
 def load_file():
-    with open('member_list.csv', 'r') as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter=',',quotechar='"')
-        next(csv_reader)
-    csvfile.close()
+    csv_reader = pd.read_csv('./config/member_list.csv')
     return csv_reader
 
 def get_github_contributes(user_id):
@@ -16,9 +13,27 @@ def get_github_contributes(user_id):
     data = data.split('\n')
     return [re.findall('data-count="([0-9]+)"', x) for x in data if re.findall('data-count="([0-9]+)"', x)]
 
+def max_contribute_count(data):
+    ret = 0
+    for num in data[::-1]:
+        if num != 0:
+            num += 1
+        else:
+            break
+    return ret
+
+def DEBUG(user, contributes):
+    print(user + ' ' + contributes)
+
 if __name__ == '__main__':
     user_data = load_file()
-    for user in user_data:
-        contributes = get_github_contributes(user[0])
-
-        for contribute in contributes[::-1]
+    for user in user_data['github_user_id']:
+        continue_contributes = 0
+        contributes = get_github_contributes(user)
+        for contribute in contributes[::-1]:
+            # print(contribute != ['0'])
+            if contribute != ['0']:
+                continue_contributes += 1
+            else:
+                break
+        print(continue_contributes)
